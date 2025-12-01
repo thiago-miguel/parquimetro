9473c65cc4812de0a0806e1b343165c4d70ee9dc
thiago-miguel <thiago.alexandre@outlook.com.br>
2025-12-01 09:13:45 -0300

    ajuste de case sensitive; aceite de virgula; foco no input; aceite de enter para calcular

--- parquimetro.js
+++ parquimetro.js
@@ -1,62 +1,70 @@
 class parquimetro {
-    constructor() {
-        this.tabela = [
-            {valor: 1.00, tempo: 30},
-            {valor: 1.75, tempo: 60},
-            {valor: 3.00, tempo: 120}
-        ];
+  constructor() {
+    this.tabela = [
+      { valor: 1.0, tempo: 30 },
+      { valor: 1.75, tempo: 60 },
+      { valor: 3.0, tempo: 120 },
+    ];
+  }
+  calcular(valorInserido) {
+    if (valorInserido < 1.0) {
+      return {
+        valido: false,
+        mensagem: "Valor insuficiente. Insira pelo menos R$ 1,00.",
+      };
     }
-    calcular(valorInserido) {
-        if (valorInserido < 1.00) {
-            return {
-                valido: false,
-                mensagem: "Valor insuficiente. Insira pelo menos R$ 1,00."
-            };
-        }
 
-        const opcaco = this.tabela.reduce((prev, atual) => {
-            return valorInserido >= atual.valor ? atual : prev;
-        });
+    const opcaco = this.tabela.reduce((prev, atual) => {
+      return valorInserido >= atual.valor ? atual : prev;
+    });
 
-        const troco = valorInserido - opcaco.valor;
+    const troco = valorInserido - opcaco.valor;
 
-        return {
-            valido: true,
-            tempo: opcaco.tempo,
-            troco: troco.toFixed(2)
-        };
-    }
+    return {
+      valido: true,
+      tempo: opcaco.tempo,
+      troco: troco.toFixed(2),
+    };
+  }
 }
 
 class Interface {
-    constructor() {
-        this.parquimetro = new parquimetro();
-        this.inputValor = document.getElementById('valor');
-        this.resultado = document.getElementById('resultado');
-        this.btn = document.getElementById('btncalcular');
+  constructor() {
+    this.parquimetro = new parquimetro();
+    this.inputValor = document.getElementById("valor");
+    this.resultado = document.getElementById("resultado");
+    this.btn = document.getElementById("btnCalcular");
 
-        this.btn.addEventListener('click', () => this.processar());
+    this.btn.addEventListener("click", () => this.processar());
+
+    this.inputValor.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        this.processar();
+      }
+    });
+
+    this.inputValor.focus();
+  }
+
+  processar() {
+    let valor = this.inputValor.value.replace(",", ".");
+    valor = parseFloat(valor);
+
+    if (isNaN(valor)) {
+      this.resultado.textContent = "Por favor, digite um valor válido.";
+      return;
     }
 
-    processar() {
-        const valor = parseFloat(this.inputValor.value);
+    const resposta = this.parquimetro.calcular(valor);
 
-        if (isNaN(valor)) {
-            this.resultado.textContent = "Por favor, digite um valor válido.";
-            return;
-        }
-
-        const resposta = this.parquimetro.calcular(valor);
-
-        if(!resposta.valido) {
-            this.resultado.textContent = resposta.mensagem;
-        } else {
-            this.resultado.innerHTML =
-            `Tempo adquirido: <strong>${resposta.tempo} minutos</strong><br>` +
-            `Troco: R$ <strong>${resposta.troco}</strong>`;
-        }
+    if (!resposta.valido) {
+      this.resultado.textContent = resposta.mensagem;
+    } else {
+      this.resultado.innerHTML =
+        `Tempo adquirido: <strong>${resposta.tempo} minutos</strong><br>` +
+        `Troco: R$ <strong>${resposta.troco}</strong>`;
     }
-
+  }
 }
 
 new Interface();
