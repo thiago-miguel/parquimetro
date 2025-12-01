7d6950b219d79e466cacb57965c2de7db1ce0a45
thiago-miguel <thiago.alexandre@outlook.com.br>
2025-12-01 08:57:53 -0300

    Criação da classe Interface

--- parquimetro.js
+++ parquimetro.js
@@ -27,3 +27,36 @@ class parquimetro {
         };
     }
 }
+
+class Interface {
+    constructor() {
+        this.parquimetro = new parquimetro();
+        this.inputValor = document.getElementById('valor');
+        this.resultado = document.getElementById('resultado');
+        this.btn = document.getElementById('btncalcular');
+
+        this.btn.addEventListener('click', () => this.processar());
+    }
+
+    processar() {
+        const valor = parseFloat(this.inputValor.value);
+
+        if (isNaN(valor)) {
+            this.resultado.textContent = "Por favor, digite um valor válido.";
+            return;
+        }
+
+        const resposta = this.parquimetro.calcular(valor);
+
+        if(!resposta.valido) {
+            this.resultado.textContent = resposta.mensagem;
+        } else {
+            this.resultado.innerHTML =
+            `Tempo adquirido: <strong>${resposta.tempo} minutos</strong><br>` +
+            `Troco: R$ <strong>${resposta.troco}</strong>`;
+        }
+    }
+
+}
+
+new Interface();
